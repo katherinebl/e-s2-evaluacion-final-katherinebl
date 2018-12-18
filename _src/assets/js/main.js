c4525adf1d4f5b6d1f8df6e656b8f334a97f6616
Katherine Briceño <kath.gbl@gmail.com>
2018-12-18 11:47:57 +0100

    clean the code

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,32 +1,29 @@
 'use strict';
 
-/* Search using API */
-
 /* Declare variables */
 const apiUrl = 'http://api.tvmaze.com/search/shows?q=';
 const input = document.querySelector('.input');
 const button = document.querySelector('.btn');
 const listResults = document.querySelector('.results');
 
+/* Search using API */
 function searchSeries (){
   //take text value from user
   let userSearch = input.value;
   //reset search each time the users starts a new search
   listResults.innerHTML=('');
-  //use the API to get the data specify by user input
+  //use the API to get the data specify by user
   fetch(apiUrl + userSearch)
     .then(response => response.json())
     .then(data => {
-      //create a variable to store the data
-      // const show = data[0].show;
+
       //go through the list of elements (loop the array)
       for (let i = 0; i < data.length; i++){
-        //store in variables the content of name and image from each show
+        //store in variables the content (name and image) from each show
         const title = data[i].show.name;
         const image = data[i].show.image;
-        // const id = data[i].show.id;
 
-        //If the result has no image, use filler image (AL PARECER FUNCIONA, REVÍSALO AL FINAL)
+        //if the result has no image, use filler image
         if (image === null) {
           const fillerImage = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
           const contentNull = `
@@ -38,7 +35,7 @@ function searchSeries (){
         } else {
           const imgMedium = data[i].show.image.medium;
           const contentMedium = `
-          <li class="series__item">
+          <li class="series__item" id="${title}">
           <img class="series__image" src="${imgMedium}" alt="${title}">
           <h2 class="series__title">${title}</h2>
           </li>`;
@@ -50,43 +47,32 @@ function searchSeries (){
 }
 button.addEventListener('click', searchSeries);
 
-//create a function to select user favorite items
+//Declares an array to add favorites before pass them to local storage
+let storedFavs = [];
+
+/* Create a function to select user favorite items (series) */
 function userFavs (event) {
   const favItem = event.currentTarget;
   favItem.classList.toggle('favs');
 
-  //store the information of user favorite items in the localStorage
+  //add favorite series to the array
+  if (favItem.classList.contains('favs')) {
+    storedFavs.push(favItem.id);
 
-
-  //Add favorite series to local storage --> SE BORRA CUANDO REINICIO
-  if (favItem.classList.contains('favs') === true) {
-    storedFavs.push(favItem.innerHTML);
-    console.log(storedFavs);
-
+  //remove favorite series to the array
   } else {
-    remove(storedFavs, favItem.innerHTML);
-    console.log(storedFavs);
+    const index = storedFavs.indexOf(favItem.id);
+    storedFavs.splice(index, 1);
   }
-
-  //Remove favorite series from local storage
-  function remove (array, element) {
-    const index = array.indexOf(element);
-
-    if (index !== -1) {
-      array.splice(index, 1);
-    }
-  }
-
+  //store the information of user favorite series in local storage
   localStorage.setItem('storedFavs', JSON.stringify(storedFavs));
-  const  savedSeries= JSON.parse(localStorage.getItem('storedFavs'));
-  console.log(savedSeries.length);
+  const savedSeries = JSON.parse(localStorage.getItem('storedFavs'));
 }
-let storedFavs = [];
 
-
+//When the user clicks an item, the item is highlighted and vice versa
 function selectedItem () {
-  const li = document.querySelectorAll('.series__item');
-  for (let i = 0; i <li.length; i++){
-    li[i].addEventListener('click', userFavs);
+  const item = document.querySelectorAll('.series__item');
+  for (let i = 0; i <item.length; i++){
+    item[i].addEventListener('click', userFavs);
   }
 }
